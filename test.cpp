1e018069cd87e0420ac971dda5d1ae0c94e116e7
Mario Merendino <mmerendino@mdsol.com>
2017-07-27 14:45:14 -0400

    modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
-
+void count(int x){
+	for(int i = 0; i < x; i++){
+		cout << i << endl;
+	}
+}
 int main(){
 	cout << "TEST" << endl;
 	cout << "CHANGE MADE IN GITHUB" << endl;
